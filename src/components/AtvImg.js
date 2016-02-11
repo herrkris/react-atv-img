3c052f1e9a145b8b92096504af94e721463c23a7
Kristof Dreier <kristof.dreier@chimpplus.com>
2016-02-11 13:11:59 +0100

    renamed layerCount to childrenCount

--- src/components/AtvImg.js
+++ src/components/AtvImg.js
@@ -31,7 +31,7 @@ export default class AtvImg extends Component {
   }
 
   handleMove = ({ pageX, pageY }) => {
-    const layerCount = this.props.children.length; // the number of children
+    const childrenCount = this.props.children.length; // the number of children
 
     const { rootElemWidth, rootElemHeight } = this.state;
 
@@ -58,7 +58,7 @@ export default class AtvImg extends Component {
         background: `linear-gradient(${angle}deg, rgba(255, 255, 255, ${(pageY - offsets.top - bodyScrollTop) / rootElemHeight * 0.4}) 0%, rgba(255, 255, 255, 0) 80%)`,
       },
       children: this.props.children.map((_, idx) => ({
-        transform: `translateX(${(offsetX * (layerCount - idx)) * ((idx * 2.5) / wMultiple)}px) translateY(${offsetY * layerCount * ((idx * 2.5) / wMultiple)}px)`,
+        transform: `translateX(${(offsetX * (childrenCount - idx)) * ((idx * 2.5) / wMultiple)}px) translateY(${offsetY * childrenCount * ((idx * 2.5) / wMultiple)}px)`,
       })),
     });
   }
